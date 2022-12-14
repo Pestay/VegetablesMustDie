42641f1409872128512eba3f7762eb4e07d9286c
Pablo Estay <pablo.estay@sansano.usm.cl>
2022-12-13 22:56:21 -0300

    cambio en el preview de la trampa

--- src/traps/PreviewsData.cs
+++ src/traps/PreviewsData.cs
@@ -17,7 +17,7 @@ namespace PreviewsData{
         public Dictionary<string, PreviewProps> data = new Dictionary<string, PreviewProps>{
             { "WoodenTable" , new PreviewProps("res://src/traps/1x1block/table.png","res://src/traps/1x1block/WoodenBlock1x1.tscn")},
             {"Detergent" , new PreviewProps("res://src/traps/detergent/detergent.png", "res://src/traps/detergent/DetergentFloor.tscn")},
-            {"Turret" , new PreviewProps("res://src/traps/turret/turret_simple.png", "res://src/traps/turret/Turret.tscn")},
+            {"Turret" , new PreviewProps("res://src/traps/turret/turret_v2.png", "res://src/traps/turret/Turret.tscn")},
             {"Spikes", new PreviewProps("res://src/traps/spikes/spikes.png", "res://src/traps/spikes/Spikes.tscn")},
         };
 
